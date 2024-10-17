d5b029fc00df3fc70917860e0bd5e6b2c9bfd750
Priyansh Shah <105874877+Priyansh4444@users.noreply.github.com>
2024-10-17 01:23:50 -0700

    feat: Adding meta tag for google

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="8zaa0jXsA-lCUhMXM_kvgahrbL3Iev_fSf8pN4GxWvw" />
         <title>powerpoint_scraper</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
