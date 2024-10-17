3b52228f77d2c25c1067fa9d6a5b3f525a56f812
Priyansh Shah <105874877+Priyansh4444@users.noreply.github.com>
2024-10-17 01:28:48 -0700

    feat: Addin tags for seo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,16 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="PowerPoint Scraper - Extract key information from PPTX files using AI for optimized data analysis." />
+        <meta name="keywords" content="PowerPoint scraper, extract data from pptx, AI, presentation, data analysis, scraping tool, pptx to text, PPTX, LLM" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="PowerPoint Scraper - AI-based PPTX Data Extraction" />
+        <meta property="og:description" content="Easily extract key information from PowerPoint files with our AI-powered scraper tool." />
+        <meta property="og:url" content="https://powerpoint-scraper.deno.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://powerpoint-scraper.deno.dev/logo.png" />
+        <title>PowerPoint Scraper - AI-based PPTX Data Extraction</title>
         <meta name="google-site-verification" content="8zaa0jXsA-lCUhMXM_kvgahrbL3Iev_fSf8pN4GxWvw" />
-        <title>powerpoint_scraper</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
